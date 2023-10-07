423aea494c23d947fc5ede740f87153ebdc5a674
daniveler <daniel01velerdas@gmail.com>
2023-10-07 17:16:52 +0200

    Finished exercise 3.6

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const app = express()
 
 let persons = [
@@ -24,8 +25,65 @@ let persons = [
     }
 ]
 
+app.use(bodyParser.json());
+
 app.get('/api/persons', (request, response) => {
     response.json(persons)
+})
+
+app.get('/api/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    }
+    else {
+        response.status(404).end()
+    }
+})
+
+app.get('/info', (request, response) => {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = currentDate.getMonth() + 1;
+    const day = currentDate.getDate();
+
+    const formattedDate = `${day}-${month}-${year}`;
+
+    response.json({
+        count: persons.length,
+        date: formattedDate
+    })
+})
+
+
+app.post('/api/persons', (request, response) => {
+    const person = request.body.person
+
+    if (!person.name) { 
+        return response.status(400).json({ error: '"name" field is mandatory'})
+    }
+    else if (!person.number) {
+        return response.status(400).json({ error: '"number" field is mandatory'})
+    }
+    else if (persons.find(p => p.name === person.name)) {
+        return response.status(400).json({ error: `name: ${person.name} is alredy saved in the phonebook`})
+    }
+
+    const id = Math.floor(Math.random() * 1000)
+    person.id = id
+
+    persons = persons.concat(person)
+
+    response.status(201).end()
+})
+
+app.delete('/api/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
 })
 
 const PORT = 3001
